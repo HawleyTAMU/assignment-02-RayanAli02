0145869f34fe42a3ecbd8a4306827ff80dd05862
RayanAli02 <rayanamirali@yahoo.com>
2024-10-03 03:24:16 +0000

    In this assignment I made the stdin and outs also creating a user that that does getenv("USER") which gets the name of the user. next I made a getcwd that gets the current working directory furthermore, made the vector arrays pid_back and pid_inter when asked to store into the arrays, then made a while loop for the whole process that has a current time and a time_str printing out the time_str user cwd and then the Shell$ NC, then past that I made a reap background process, in this part of the program it chekcs for any background process in the pid_back and reaps(removes) them if they have completed the waitpid part in the forloop, this makes it non-blocking, so it doesnt wait for the process to finish if it's still running doing that for both the pid_back and for the pid_inter, then if the user exits then the shell waits for all the background processes to finish using waitpid then exits, furthermore, I created a fork that check if its 0 then goes through the child process and another vector char array args that will contain the commands and the arguments as strings, the loop converts them into string, then the .push_back(nillptr) adds the nullptr at the end marking the end of the argument list then if the command has input redirection, the hasInput checks for that, then freopen replaces the standard input with the specified file in_file meaning that anything read by the process will now come from that file and then the next if is the same but with hasOutput and checks that method and when doing freopen replaces the standard output with the specified file in out_file meaning that anything written to cout will go into that file instead of the terminal finally towards the end we do an if statement it checks if the current command should be run in the background or not, then the parent process waits for hte child process to finish execution. the &status stores the child's exit status if the child process's status is greater than 1 meaning it didnt get executed properly then the process ID of the child is added to the pid_back vector then regardless of whether the process runs in the foreground or background, its process ID is added to the pid_inter vector and these processes are later needed to be reaped then we do dup2 for the stdin and stdout, stdin replaces file descriptor 0 with the original stdin and stdout restores the standard output to its original state then fclose the stdin and fclose stdout because at the beginning I did FILE* stdin and stdout so I can't use regular close

--- shell.cpp
+++ shell.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
-
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-
 #include <vector>
 #include <string>
-
+#include <ctime>
 #include "Tokenizer.h"
 
 // all the basic colours for a shell prompt
@@ -20,18 +18,71 @@
 using namespace std;
 
 int main () {
-    for (;;) {
+    // use dup(...) to perserve standard in and out
+    FILE* stdin = fdopen(dup(0), "r");
+    FILE* stdout = fdopen(dup(1), "w");
+
+
+    // get the name of the user 
+    //      use getenv(...)
+    char* user = getenv("USER");
+
+
+    // get the previous working directory
+    //      use getcwd(...)
+    char cwd[1024];
+    string prev_cwd;
+    getcwd(cwd, sizeof(cwd));
+
+    // create an array or vector to store the pids of the background processes
+    vector<pid_t> pid_back;
+
+    // create an array or vector to store the pids of interrmediate processes
+    vector<pid_t> pid_inter;
+
+    while(true) {
         // need date/time, username, and absolute path to current dir
-        cout << YELLOW << "Shell$" << NC << " ";
+        time_t curr_time = time(nullptr);
+        char* time_str = ctime (&curr_time);
+        cout << YELLOW << time_str << " "<< user << ":"<<cwd <<"Shell$" << NC;
         
         // get user inputted command
         string input;
         getline(cin, input);
 
-        if (input == "exit") {  // print exit message and break out of infinite loop
+        // reap background processes
+        for (auto pid = pid_back.begin(); pid != pid_back.end();){
+            if (waitpid(*pid, nullptr, WNOHANG) > 0){ // using WNOHANG makes it not wait
+                cout << GREEN << "Reaped background process with PID: " << *pid << NC << endl;    //print output
+                pid = pid_back.erase(pid);
+            }
+            else{
+                pid++;
+            }
+        }
+        for (auto pid = pid_inter.begin(); pid != pid_inter.end();){
+            if (waitpid(*pid, nullptr, WNOHANG) > 0){ // using WNOHANG makes it not wait
+                cout << GREEN << "Reaped background process with PID: " << *pid << NC << endl;    //print output
+                pid = pid_inter.erase(pid);
+            }
+            else{
+                pid++;
+            }
+        }
+        if (input == "exit") { 
+            // print exit message and break out of infinite loop
+            // reap all remaining background processes
+            //  similar to previous reaping, but now wait for all to finish
+            //      in loop use argument of 0 instead of WNOHANG
+
             cout << RED << "Now exiting shell..." << endl << "Goodbye" << NC << endl;
+            for(pid_t pid : pid_back){
+                waitpid(pid, nullptr, 0);
+            }
             break;
         }
+
+        // [optional] do dollar sign expansion
 
         // get tokenized commands from user input
         Tokenizer tknr(input);
@@ -39,43 +90,115 @@ int main () {
             continue;
         }
 
-        // // print out every command token-by-token on individual lines
-        // // prints to cerr to avoid influencing autograder
-        // for (auto cmd : tknr.commands) {
-        //     for (auto str : cmd->args) {
-        //         cerr << "|" << str << "| ";
-        //     }
-        //     if (cmd->hasInput()) {
-        //         cerr << "in< " << cmd->in_file << " ";
-        //     }
-        //     if (cmd->hasOutput()) {
-        //         cerr << "out> " << cmd->out_file << " ";
-        //     }
-        //     cerr << endl;
-        // }
+            // print out every command token-by-token on individual lines
+            // prints to cerr to avoid influencing autograder
+        //for (auto cmd : tknr.commands) {
+        //    cerr<< "Command: ";
+        //    for (auto str : cmd->args) {
+        //        cerr << "|" << str << "| ";
+        //    }
+        //    if (cmd->hasInput()) {
+        //        cerr << "in< " << cmd->in_file << " ";
+        //    }
+        //    if (cmd->hasOutput()) {
+        //        cerr << "out> " << cmd->out_file << " ";
+        //    }
+        //    cerr << endl;
+        //}
 
-        // fork to create child
-        pid_t pid = fork();
-        if (pid < 0) {  // error check
-            perror("fork");
-            exit(2);
+        // handle directory processing
+        if (tknr.commands.size() == 1 && tknr.commands.at(0)->args.at(0) == "cd") {
+            // use getcwd(...) and chdir(...) to change the path
+            // if the path is "-" then print the previous directory and go to it
+            // otherwise just go to the specified directory
+            const string& path = tknr.commands.at(0)->args.at(1);
+            if(path == "-"){
+                cout << prev_cwd << endl;
+                chdir(prev_cwd.c_str());
+            }
+            else{
+                prev_cwd = cwd;
+                if(chdir(path.c_str())==0){
+                    getcwd(cwd, sizeof(cwd));
+                }
+                else{
+                    perror("cd failed");
+                }
+            }
+            continue;
         }
 
-        if (pid == 0) {  // if child, exec to run command
-            // run single commands with no arguments
-            char* args[] = {(char*) tknr.commands.at(0)->args.at(0).c_str(), nullptr};
+        // loop over piped commands
+        for (size_t i = 0; i < tknr.commands.size(); i++) {
+            // get the current command
+            Command* currentCommand = tknr.commands.at(i);
 
-            if (execvp(args[0], args) < 0) {  // error check
-                perror("execvp");
+            // pipe if the command is piped
+            //  (if the number of commands is > 1)
+            // fork to create child
+            pid_t pid = fork();
+            if (pid < 0) {  // error check
+                perror("fork");
                 exit(2);
             }
-        }
-        else {  // if parent, wait for child to finish
-            int status = 0;
-            waitpid(pid, &status, 0);
-            if (status > 1) {  // exit if child didn't exec properly
-                exit(status);
+
+            if (pid == 0) {  // if child, exec to run command
+                // run single commands with no arguments
+                // change to instead create an array from the vector of arguments
+                vector<char*> args;
+                for(auto& arg : currentCommand->args){
+                    args.push_back((char*)arg.c_str());                    
+                }
+                args.push_back(nullptr);
+
+                // duplicate the standard out for piping
+
+                // do I/O redirection  
+                // the Command class has methods hasInput(), hasOutput()
+                //     and member variables in_file and out_file
+
+                if(currentCommand->hasInput()){
+                    freopen(currentCommand->in_file.c_str(), "r", stdin);
+                }
+                else if(currentCommand->hasOutput()){
+                    freopen(currentCommand->out_file.c_str(), "w", stdout);
+                }
+                
+                if (execvp(args[0], args.data()) < 0) {  // error check
+                    perror("execvp");
+                    exit(2);
+                }
+            }
+            else {  // if parent, wait for child to finish
+                // if the current command is a background process then store it in the vector
+                //  can use currentCommand.isBackground()
+
+                // else if the last command, wait on the last command pipline
+                int status = 0;
+                if(!currentCommand->isBackground()){
+                    waitpid(pid, &status, 0);
+                    if (status > 1) {  // exit if child didn't exec properly
+                        exit(status);
+                    }
+                    // else add the process to the intermediate processes
+                    }
+                    else{
+                        pid_back.push_back(pid);
+                    }
+                // dup standardin
+                pid_inter.push_back(pid);
             }
         }
+
+        // restore standard in and out using dup2(...)
+        dup2(fileno(stdin), 0);
+        dup2(fileno(stdout), 1);
+
+        // print out the reaped background processes
+
+        // reap intermediate processes
     }
+    fclose(stdin);
+    fclose(stdout);
+    return 0;
 }
